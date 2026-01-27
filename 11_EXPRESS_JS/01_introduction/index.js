b4706e0461cd86236630325a3aadd362557e03be
SanjayMaity15 <sanjaymaityofficial15@gmail.com>
2026-01-27 07:24:17 +0530

    Middleware learned

---
+++ 11_EXPRESS_JS/01_introduction/index.js
@@ -0,0 +1,42 @@
+import express from "express"
+
+const app = express()
+app.disable("x-powered-by")
+
+const PORT = 3000;
+
+
+app.get(
+	"/",
+	(req, res, next) => {
+		res.setHeader("Content-Type", "text/html");
+		res.write("Hello World 1");
+		next();
+	},
+    (req, res) => {
+        throw new Error("Error")
+		res.end("Hello World 2");
+        
+	},
+	(err, req, res, next) => {
+		
+		res.end("Error");
+	},
+);
+app.listen(PORT, () => {
+    console.log("SERVER STARTED AT PORT", PORT);
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
