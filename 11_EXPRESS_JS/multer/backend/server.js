0474614c78a415103595b17d52cb87eeee8f52e7
SanjayMaity15 <sanjaymaityofficial15@gmail.com>
2026-01-28 16:32:06 +0530

    Learned multer

---
+++ 11_EXPRESS_JS/multer/backend/server.js
@@ -0,0 +1,29 @@
+import express from "express";
+import cors from "cors";
+import multer from "multer";
+
+const app = express();
+app.use(cors());
+
+const storage = multer.diskStorage({
+	destination: function (req, file, cb) {
+		cb(null, "./store");
+	},
+	filename: function (req, file, cb) {
+		cb(null, file.originalname);
+	},
+});
+
+const upload = multer({ storage: storage });
+
+app.post("/send", upload.single("profile"), (req, res) => {
+    console.log(req.file);
+    console.log(req.body)
+	res.send("File uploaded successfully");
+});
+
+app.get("/", (req, res) => {
+	res.send("Server is running");
+});
+
+app.listen(8000, () => console.log("Server is running "));
