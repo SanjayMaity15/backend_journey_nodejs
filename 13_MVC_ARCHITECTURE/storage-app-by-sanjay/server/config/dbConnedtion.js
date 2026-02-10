5413217bd134aec836f2404540175718f25f6615
SanjayMaity15 <sanjaymaityofficial15@gmail.com>
2026-02-10 06:31:04 +0530

    implementing mvc architecture

---
+++ 13_MVC_ARCHITECTURE/storage-app-by-sanjay/server/config/dbConnedtion.js
@@ -0,0 +1,18 @@
+import { MongoClient } from "mongodb";
+
+export const client = new MongoClient("mongodb://localhost:27017/storageApp")
+
+export const connectDB = async () => {
+    await client.connect()
+
+    const db = client.db();
+
+    return db;
+}
+
+
+process.on("SIGINT", async () => {
+    await client.close()
+    console.log("Client Disconnected")
+    process.exit(0)
+})
