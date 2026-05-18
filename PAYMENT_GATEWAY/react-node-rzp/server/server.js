57b5d48f0ebcb50756b01bb31dc06635f002ca43
SanjayMaity15 <sanjaymaityofficial15@gmail.com>
2026-05-18 14:14:09 +0530

    payment gateway implementation

--- PAYMENT_GATEWAY/react-node-rzp/server/server.js
+++ PAYMENT_GATEWAY/react-node-rzp/server/server.js
@@ -1,7 +1,7 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
-import { rzp } from "./config/rzp.js";
+// import { rzp } from "./config/rzp.js";
 
 dotenv.config();
 
@@ -15,44 +15,44 @@ app.use(
 );
 app.use(express.json());
 
-app.post("/payment", async (req, res) => {
-	try {
-		const { amountToPay } = req.body;
+// app.post("/payment", async (req, res) => {
+// 	try {
+// 		const { amountToPay } = req.body;
 
-		const order_id = await rzp.orders.create({
-			amount: amountToPay * 100,
-			currency: "INR",
-		});
+// 		const order_id = await rzp.orders.create({
+// 			amount: amountToPay * 100,
+// 			currency: "INR",
+// 		});
 
-		res.status(200).json({
-			message: "Successful",
-			data: order_id,
-		});
-	} catch (error) {
-		res.status(400).json({
-			message: "Server error",
-		});
-	}
-});
+// 		res.status(200).json({
+// 			message: "Successful",
+// 			data: order_id,
+// 		});
+// 	} catch (error) {
+// 		res.status(400).json({
+// 			message: "Server error",
+// 		});
+// 	}
+// });
 
-app.post("/verify-payment", async (req, res) => {
-	try {
-		const { razorpay_order_id } = req.body;
+// app.post("/verify-payment", async (req, res) => {
+// 	try {
+// 		const { razorpay_order_id } = req.body;
 
-		const order = await rzp.orders.fetch(razorpay_order_id);
+// 		const order = await rzp.orders.fetch(razorpay_order_id);
 
-		if (order.status === "paid") {
-			return res.status(200).json({
-				message: "Successful",
-				data: order,
-			});
-		}
-	} catch (error) {
-		res.status(400).json({
-			message: "Server error",
-		});
-	}
-});
+// 		if (order.status === "paid") {
+// 			return res.status(200).json({
+// 				message: "Successful",
+// 				data: order,
+// 			});
+// 		}
+// 	} catch (error) {
+// 		res.status(400).json({
+// 			message: "Server error",
+// 		});
+// 	}
+// });
 
 app.get("/", (req, res) => {
 	res.send("Server is fine");
